f8764007c0ff3f29fe31f33fa0d8c66d934895e1
HaoChuan9421 <hc199421@gmail.com>
2021-04-06 23:36:37 +0800

    feat: router 不拆异步,方便在 IE 下调试

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import About from '../views/About.vue';
 
 Vue.use(VueRouter);
 
@@ -13,7 +14,7 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    component: () => import('../views/About.vue')
+    component: About
   }
 ];
 
